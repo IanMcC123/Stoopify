dd10ac67411647a28d5b79658aa6d94ff7f5527b
Safin Shihab <shihab3750@gmail.com>
2024-02-18 20:14:38 -0500

    Added font, color, alignment

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,27 @@
 // Header Render
-import './header.css'
+import "./header.css";
 
 export default function Header() {
-    return (
-        <header>
-            TODO: Add Header Structure + Styling
-        </header>
-    )
+  return (
+    // NavBar
+    <div>
+      <section id="navBar">
+        <header>Stoopify</header>
+        <div id="search-container">
+          <input id="search-bar" type="text" placeholder="Search Furniture" />
+          {/* Add search Icon here */}
+        </div>
+        <div id="login">
+          <h3 style={{ fontWeight: 600, fontSize: 30 }}>Login</h3>
+          {/* <img src="Login.png" alt="Login Icon"></img> */}
+          {/* Image isn't loading. gotta fix */}
+        </div>
+      </section>
+      <section id="menu-items">
+        <h3>Decor</h3>
+        <h3 style={{ marginRight: "5%", marginLeft: "5%" }}>Furniture</h3>
+        <h3>About Us</h3>
+      </section>
+    </div>
+  );
 }
