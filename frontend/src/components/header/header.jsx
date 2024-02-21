f8f781ccbeeadb23d181236b871a6bc9bb792163
Safin Shihab <shihab3750@gmail.com>
2024-02-21 13:38:33 -0500

    Added login and down arrow icons

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,6 +1,9 @@
 // Header Render
 import "./header.css";
 
+import login from "../assets/header/Login.png";
+import DownArrow from "../assets/header/Down_Arrow.png";
+
 export default function Header() {
   return (
     // NavBar
@@ -12,15 +15,49 @@ export default function Header() {
           {/* Add search Icon here */}
         </div>
         <div id="login">
-          <h3 style={{ fontWeight: 600, fontSize: 30 }}>Login</h3>
-          {/* <img src="Login.png" alt="Login Icon"></img> */}
-          {/* Image isn't loading. gotta fix */}
+          <h3 style={{ fontWeight: 600, fontSize: 30 }}>
+            <img
+              src={login}
+              alt="Login.png"
+              width="30"
+              height="30"
+              style={{ marginRight: 10 }}
+            ></img>
+            Login
+          </h3>
         </div>
       </section>
       <section id="menu-items">
-        <h3>Decor</h3>
-        <h3 style={{ marginRight: "5%", marginLeft: "5%" }}>Furniture</h3>
-        <h3>About Us</h3>
+        <h3>
+          Decor
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
+        <h3 style={{ marginRight: "5%", marginLeft: "5%" }}>
+          Furniture
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
+        <h3>
+          About Us
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
       </section>
     </div>
   );
