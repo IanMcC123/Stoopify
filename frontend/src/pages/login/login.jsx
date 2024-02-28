a8f0e441836d14b5e4754e1e181d63dd308f483b
George-Hachem <georgeeliashachem@gmail.com>
2024-02-28 12:54:19 -0500

    Updated the login page

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,78 +1,79 @@
-// import React from 'react';
-// import axios from 'axios';
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios';
 
-// export default function Login() {
-//     const history = useNavigate();
-//     const [state, setState] = useState({
-//         username: "",
-//         password: ""
-//       });
+export default function Login() {
+    const history = useNavigate();
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+      });
     
-//       const handleChange = (fieldName) => (e) => {
-//         const value = e.target.value;
-//         setState({
-//           ...state,
-//           [fieldName]: value
-//         });
-//       };
+      const handleChange = (fieldName) => (e) => {
+        const value = e.target.value;
+        setState({
+          ...state,
+          [fieldName]: value
+        });
+      };
     
-//       const handleSubmit = (e) => {
-//         e.preventDefault();
-//         const userData = {
-//           username: state.username,
-//           password: state.password
-//         };
-//         ApiClient.post("/api/auth/signin", userData).then((response) => {
-//           console.log(response);
-//           history('/feed');
-//         })
-//         .catch((error) => {
-//           if (error.response) {
-//             console.log(error.response);
-//             console.log("server responded");
-//           } else if (error.request) {
-//             console.log("network error");
-//           } else {
-//             console.log(error);
-//           }
-//         });
-//       };
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const userData = {
+          username: state.username,
+          password: state.password
+        };
+        axios.post("/api/auth/signin", userData).then((response) => {
+          console.log(response);
+          history('/feed');
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response);
+            console.log("server responded");
+          } else if (error.request) {
+            console.log("network error");
+          } else {
+            console.log(error);
+          }
+        });
+      };
     
-//     return (
-//     <form onSubmit={handleSubmit}>
-//         <div className="container">
-//         <div className="box">
-//             <div className="signTitle">
-//             <h2>Sign In</h2><br />
-//             </div>
+    return (
+    <form onSubmit={handleSubmit}>
+        <div className="container">
+        <div className="box">
+            <div className="signTitle">
+            <h2>Sign In</h2><br />
+            </div>
             
-//             <div className="email">
-//                 <input 
-//                 className = "textSign" 
-//                 type="text" 
-//                 name = "username"
-//                 value = {state.username}
-//                 onChange = {handleChange('username')}
-//                 placeholder="username"
-//                 required/> 
-//             </div>
-//             <div className="password">
-//                 <input 
-//                 className = "textSign" 
-//                 type="text" 
-//                 name = "Password"
-//                 value = {state.password}
-//                 onChange = {handleChange('password')} 
-//                 placeholder="Password" 
-//                 required/>
-//             </div>
-//             <div className="submitButton">
-//                 {/* <input type="submit" value = "Login" /> */}
-//                 <button id='signUpB' type = "submit">Sign in</button>
-//             </div>
-//         </div>     
-//         </div>
-//     </form>
+            <div className="email">
+                <input 
+                className = "textSign" 
+                type="text" 
+                name = "username"
+                value = {state.username}
+                onChange = {handleChange('username')}
+                placeholder="username"
+                required/> 
+            </div>
+            <div className="password">
+                <input 
+                className = "textSign" 
+                type="text" 
+                name = "Password"
+                value = {state.password}
+                onChange = {handleChange('password')} 
+                placeholder="Password" 
+                required/>
+            </div>
+            <div className="submitButton">
+                {/* <input type="submit" value = "Login" /> */}
+                <button id='signUpB' type = "submit">Sign in</button>
+            </div>
+        </div>     
+        </div>
+    </form>
         
-//     )
-// }
+    )
+}
