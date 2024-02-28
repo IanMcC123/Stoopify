93c52bb33da9e0401abc8e4fae41a6113858ea0c
IanMcC123 <ianmccullough01@gmail.com>
2024-02-28 12:45:34 -0500

    Leftovers

--- frontend/src/pages/profile/profile.jsx
+++ frontend/src/pages/profile/profile.jsx
@@ -6,13 +6,31 @@ export default function Profile() {
     return (
         <>
             <Header />
-            <div className="Profile">
-            <p>
-                This is the Profile Page
-            </p>
-            <p>
-                Profile Content
-            </p>
+            <div className="profile-main">
+                <div class="profile-grid-wrapper" >
+                    <div class="profile-grid">
+                        <div class="profile-cell">
+                            <div  class="cell-child">
+                                <img src="./profile-pic.png" alt="add profile pic icon"/>
+                                <input class="input" placeholder="Please Enter Text" type="text" ></input>
+                                <p>Pronouns</p>
+                                <p>Member Since</p>
+                            </div>
+                        </div>
+                        <div class="profile-cell">
+                            <div class="cell-child">
+                                <p>Phone #</p>
+                                <p>Email</p>
+                                <p>Primary Zip Code</p>
+                            </div>
+                            <div class="cell-child">
+                                <p>Seller Rating</p>
+                                <p># items sold</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             <a href="/">Return to Home</a>
             </div>
             <Footer />
