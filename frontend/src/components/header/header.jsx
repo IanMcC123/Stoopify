5adf6d4b9ac1f7d2ca829e56fa00cae5e57e5321
Safin Shihab <78178724+shihab3750@users.noreply.github.com>
2024-02-28 12:44:44 -0500

    Feature fe header (#7)
    * Added the pngs for the icons
    
    * Added font, color, alignment
    
    * Added login and down arrow icons
    
    * Added search icon and fixed css for nav bar
    
    * Added comments

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,76 @@
 // Header Render
-import './header.css'
+import "./header.css";
+import login from "../assets/header/Login.png";
+import DownArrow from "../assets/header/Down_Arrow.png";
+import searchIcon from "../assets/header/Search_Icon.png";
 
 export default function Header() {
-    return (
-        <header>
-            TODO: Add Header Structure + Styling
-        </header>
-    )
+  return (
+    <div>
+      {/* Contains all components of the navBar inside a flexbox */}
+      <section id="navBar">
+        {/* Stoopify Name/logo*/}
+        <header>Stoopify</header>
+
+        <div id="search-container">
+          {/* Search component of the navBar */}
+          <input
+            id="search-bar"
+            type="text"
+            placeholder="Search Furniture..."
+          />
+          <div id="search-icon-container">
+            <img src={searchIcon} alt="Search" width="20" height="20" />
+          </div>
+        </div>
+        <div id="login">
+          {/* Login component of the navBar */}
+          <h3 style={{ fontWeight: 600, fontSize: 30 }}>
+            <img
+              src={login}
+              alt="Login.png"
+              width="30"
+              height="30"
+              style={{ marginRight: 10 }}
+            ></img>
+            Login
+          </h3>
+        </div>
+      </section>
+
+      {/* Added another section right below navBar to contain the menu items also in flexbox */}
+      <section id="menu-items">
+        <h3>
+          Decor
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
+        <h3 style={{ marginRight: "5%", marginLeft: "5%" }}>
+          Furniture
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
+        <h3>
+          About Us
+          <img
+            src={DownArrow}
+            style={{ marginLeft: 5 }}
+            alt="Down_Arrow.png"
+            width="10"
+            height="7"
+          ></img>
+        </h3>
+      </section>
+    </div>
+  );
 }
