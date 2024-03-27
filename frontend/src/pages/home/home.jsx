cf394582011a31a3c352922df056aa0c5bb4f4a4
Safin Shihab <shihab3750@gmail.com>
2024-03-27 13:15:14 -0400

    Added baseline for the homepage.

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,21 +1,21 @@
-import './home.css';
-import Footer from '../../components/footer/footer'
-import Header from '../../components/header/header'
+import "./home.css";
+import Footer from "../../components/footer/footer";
+import Header from "../../components/header/header";
 
 export default function Home() {
-    return (
-      <>
-        <Header />
-        <div class="home-content-wrapper">
-          <p>
-            This is The Home Page
-          </p>
-          <p>
-            Home Page Content
-          </p>
-          <a href="/profile">Profile Page</a>
+  return (
+    <>
+      <Header />
+      <div class="home-content-wrapper">
+        <div id="title">Latest Listings</div>
+        <div id="transparent-wrap">
+          <div class="item">Item 1</div>
+          <div class="item">Item 2</div>
+          <div class="item">Item 3</div>
         </div>
-        <Footer />
-      </>
-    )
+      </div>
+
+      <Footer />
+    </>
+  );
 }
